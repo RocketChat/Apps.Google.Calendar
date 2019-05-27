7e326eff4d4e80e5acc8c1813e7e82ef49325ade
Kapil <gautime.idk@gmail.com>
2019-05-27 19:31:30 +0530

    Initial Commit

---
+++ GoogleCalendar.ts
@@ -0,0 +1,18 @@
+import {
+    IAppAccessors,
+    ILogger,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { SettingType } from '@rocket.chat/apps-engine/definition/settings';
+
+
+export class GoogleCalendarApp extends App {
+
+    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+       
+    }
+
+
+}
