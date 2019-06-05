120332208d6cce69bc8648bad82ea9696a7e7fc7
Kapil <gautime.idk@gmail.com>
2019-06-06 03:31:43 +0530

    Updated api call

--- helpers/Webhook.ts
+++ helpers/Webhook.ts
@@ -14,21 +14,22 @@ export class WebhookEndpoint extends ApiEndpoint {
     private readonly secret = '-lYglmNGqFNNazKoQX1m-EC9';
     public tokenid;
 
-    public async post(request: IApiRequest, persist: IPersistence, logger: ILogger, http: IHttp, modify: IModify, context: SlashCommandContext): Promise<IApiResponse> {
+    public async get(request: IApiRequest,endpoint:IApiEndpointInfo, read:IRead, modify: IModify, http: IHttp,persist: IPersistence): Promise<IApiResponse> {
 
-        logger.debug('response from first request is:', request.params);
+        //logger.debug('response from first request is:', request.params);`
 
-        const auth_code = request.params.code;
-        const newresponse = await http.post(`https://www.googleapis.com/oauth2/v4/token/code=${auth_code}&client_id=${this.Client_id}&client_secret=${this.secret}&redirect_uri=http://localhost:3000/general&grant_type=authorization_code`);
+        const auth_code = request.query.code;
+        const url='https://www.googleapis.com/oauth2/v4/token';
+        const newresponse = await http.post(url, { params: {'code':`${auth_code}`,'client_id':`${this.Client_id}`,'client_secret':`${this.secret}`,'redirect_uri':'http://localhost:3000/general','grant_type':'authorization_code',}});
 
         if (newresponse.statusCode !== HttpStatusCode.OK || !newresponse.data) {
-            logger.debug('Did not get a valid response', newresponse);
-            throw new Error('Unable to retrieve response with auth code.');
+           console.log('Did not get a valid response', newresponse);
+         throw new Error('Unable to retrieve response with auth code.');
         }
 
 
         const acesstoken = new GCResults(newresponse.data);
-        logger.debug('Auth token received is: ', auth_code);
+       // logger.debug('Auth token received is: ', auth_code);
         this.tokenid = persist.create(acesstoken);
         return this.success();
 
