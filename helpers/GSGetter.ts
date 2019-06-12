b3d7ee87ff6b46c557b92883dd8cf739b53b28ec
Kapil <gautime.idk@gmail.com>
2019-06-12 22:35:36 +0530

    Added logout feature

--- helpers/GSGetter.ts
+++ helpers/GSGetter.ts
@@ -33,8 +33,6 @@ export class GCGetter {
         const parame = phase;
 
         if (parame == 'auth') {
-            //step1,2,3
-            //  const auth_code = await this.app.getwebhook().stepone(req,end,this.app.getLogger(), http,modify,context);
             const msg = modify.getCreator().startMessage().setSender(context.getSender()).setRoom(context.getRoom());
             const response = (`${this.urli}client_id=${Client_id}&redirect_uri=http://localhost:3000/api/apps/public/c759c4f1-a3c1-4202-8238-c6868633ed87/webhook&scope=https://www.googleapis.com/auth/calendar.readonly&prompt=consent&access_type=offline&response_type=code`);
 
@@ -54,8 +52,20 @@ export class GCGetter {
         }
 
         if (parame == 'logout') {
-            const resnew = await http.get(`https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost:3000`);
-       
+
+            const msg = modify.getCreator().startMessage().setSender(context.getSender()).setRoom(context.getRoom());
+            const response = `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost:3000`;
+            try {
+                msg.setText(response);
+                await modify.getCreator().finish(msg);
+            } catch (e) {
+                this.app.getLogger().error('Failed sending login url', e);
+                msg.setText('An error occurred when trying to send the login url:disappointed_relieved:');
+
+                modify.getNotifier().notifyUser(context.getSender(), msg.getMessage());
+            }
+
+
             const atoken = await persistence.getAT(context.getSender());
 
             console.log('This is the access token inside GCGetter:', atoken);
